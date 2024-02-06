c59f7a210517ef4dea1f0a3038072729e5bd5814
MaminSiberian <120947225+msiberian42@users.noreply.github.com>
2024-02-06 20:42:32 +0300

    Поправил логику преследования

--- Assets/GameResources/Features/Enemies/Scripts/EnemyMovementController.cs
+++ Assets/GameResources/Features/Enemies/Scripts/EnemyMovementController.cs
@@ -14,12 +14,16 @@
         [SerializeField, Header("Максимальная дистанция, на которую враг приближается к игроку")]
         protected float followDistance = 5f;
 
-        [SerializeField, Header("Частота обновления информации о позиции о")]
+        [SerializeField, Header("Частота обновления информации о позиции игрока")]
         protected float playerPosUpdateRate = 1f;
+
+        [SerializeField]
+        protected LayerMask wallsLayer = default;
 
         protected NavMeshAgent agent = default;
         protected PlayerMovementController player = default;
         protected Coroutine checkPlayerPositionRoutine = default;
+        protected Vector3 lastPlayerPos = default;
 
         protected virtual void Awake()
         {
@@ -48,16 +52,50 @@
         {
             while (isActiveAndEnabled)
             {
-                if (Vector3.Distance(transform.position, player.transform.position) >= followDistance)
+                if (PlayerIsVisible())
+                {
+                    lastPlayerPos = player.transform.position;
+
+                    if (Vector3.Distance(transform.position, player.transform.position) >= followDistance)
+                    {
+
+                        agent.SetDestination(lastPlayerPos);
+
+                    }
+                    else if (Vector3.Distance(transform.position, player.transform.position) < followDistance)
+                    {
+                        agent.SetDestination(transform.position);
+                    }
+                }
+                else if (lastPlayerPos != default && Vector3.Distance(transform.position, lastPlayerPos) >= 1f)
+                {
+                    agent.SetDestination(lastPlayerPos);
+                }
+                else if (lastPlayerPos != default)
                 {
                     agent.SetDestination(player.transform.position);
+                    lastPlayerPos = default;
                 }
-                else
-                {
-                    agent.SetDestination(transform.position);
-                }
+
                 yield return new WaitForSeconds(playerPosUpdateRate);
             }
+        }
+
+        protected virtual bool PlayerIsVisible()
+        {
+            RaycastHit2D hit = Physics2D.Linecast(transform.position, player.transform.position, wallsLayer);
+
+            if (hit.collider != null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        protected virtual void Rotate(Vector3 target)
+        {
+
         }
     }
 }
