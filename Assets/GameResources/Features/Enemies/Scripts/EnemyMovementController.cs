e18b05d51effda1d353f73e9ecb91167efb2862f
MaminSiberian <120947225+msiberian42@users.noreply.github.com>
2024-02-06 17:40:50 +0300

    Сделал преследование игрока

---
+++ Assets/GameResources/Features/Enemies/Scripts/EnemyMovementController.cs
@@ -0,0 +1,63 @@
+﻿namespace Tanks.Features.Enemies
+{
+    using System.Collections;
+    using Tanks.Features.Player;
+    using UnityEngine;
+    using UnityEngine.AI;
+
+    /// <summary>
+    /// Контроллер передвижения врага
+    /// </summary>
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class EnemyMovementController : MonoBehaviour
+    {
+        [SerializeField, Header("Максимальная дистанция, на которую враг приближается к игроку")]
+        protected float followDistance = 5f;
+
+        [SerializeField, Header("Частота обновления информации о позиции о")]
+        protected float playerPosUpdateRate = 1f;
+
+        protected NavMeshAgent agent = default;
+        protected PlayerMovementController player = default;
+        protected Coroutine checkPlayerPositionRoutine = default;
+
+        protected virtual void Awake()
+        {
+            agent = GetComponent<NavMeshAgent>();
+            agent.updateRotation = false;
+            agent.updateUpAxis = false;
+
+            player = FindAnyObjectByType<PlayerMovementController>();
+        }
+
+        protected virtual void OnEnable()
+        {
+            checkPlayerPositionRoutine = StartCoroutine(CheckPlayerPositionRoutine());
+        }
+
+        protected virtual void OnDisable()
+        {
+            if (checkPlayerPositionRoutine != null)
+            {
+                StopCoroutine(checkPlayerPositionRoutine);
+                checkPlayerPositionRoutine = null;
+            }
+        }
+
+        protected virtual IEnumerator CheckPlayerPositionRoutine()
+        {
+            while (isActiveAndEnabled)
+            {
+                if (Vector3.Distance(transform.position, player.transform.position) >= followDistance)
+                {
+                    agent.SetDestination(player.transform.position);
+                }
+                else
+                {
+                    agent.SetDestination(transform.position);
+                }
+                yield return new WaitForSeconds(playerPosUpdateRate);
+            }
+        }
+    }
+}
