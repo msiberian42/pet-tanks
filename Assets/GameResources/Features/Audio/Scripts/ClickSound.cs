80ac9857c08d1430d1a34512a137d82c3dede68d
MaminSiberian <120947225+msiberian42@users.noreply.github.com>
2024-02-10 12:24:10 +0300

    Звук нажатия на кнопку в меню

---
+++ Assets/GameResources/Features/Audio/Scripts/ClickSound.cs
@@ -0,0 +1,18 @@
+﻿namespace Tanks.Features.Audio
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// Проигрывание звука при нажатии на кнопку
+    /// </summary>
+    public class ClickSound : MonoBehaviour
+    {
+        [SerializeField]
+        protected AudioSource sound = default;
+
+        /// <summary>
+        /// Проиграть клик
+        /// </summary>
+        public virtual void PlayClickSound() => sound.Play();
+    }
+}
