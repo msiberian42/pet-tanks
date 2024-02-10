34f74553e2b6532dcedf9776b2501777c995dac5
MaminSiberian <120947225+msiberian42@users.noreply.github.com>
2024-02-10 10:42:24 +0300

    Сделал кнопки в меню

---
+++ Assets/GameResources/Features/UI/BaseButton.cs
@@ -0,0 +1,24 @@
+﻿namespace Tanks.Features.UI
+{
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    /// <summary>
+    /// Контроллер кнопки
+    /// </summary>
+    [RequireComponent(typeof(Button))]
+    public abstract class BaseButton : MonoBehaviour
+    {
+        protected Button button;
+
+        protected virtual void Awake()
+        {
+            button = GetComponent<Button>();
+            button.onClick.AddListener(OnButtonClicked);
+        }
+
+        protected virtual void OnDestroy() => button.onClick.RemoveListener(OnButtonClicked);
+
+        protected abstract void OnButtonClicked();
+    }
+}
