10b3924dbeb1476bdcd36d0dab231d4b33c3cb1d
MaminSiberian <120947225+msiberian42@users.noreply.github.com>
2024-02-11 17:30:36 +0300

    Сделал мины

---
+++ Assets/GameResources/Features/Enviroment/Mines/MineController.cs
@@ -0,0 +1,55 @@
+﻿namespace Tanks.Features.Enviroment
+{
+    using UnityEngine;
+    using Tanks.Features.Enemies;
+    using Tanks.Features.Player;
+    using Tanks.Features.Explosion;
+
+    /// <summary>
+    /// Контроллер мины
+    /// </summary>
+    [RequireComponent(typeof(Collider2D))]
+    public class MineController : MonoBehaviour
+    {
+        [SerializeField]
+        protected float explosionDamage = 80f;
+
+        [SerializeField]
+        protected float explosionRadius = 2f;
+
+        protected ExplosionsPool explosionsPool = default;
+        protected ExplosionController explosionController = default;
+        protected PlayerMovementController player = default;
+        protected EnemyBehaviourController enemy = default;
+
+        protected virtual void Awake() => explosionsPool = FindAnyObjectByType<ExplosionsPool>();
+
+        protected void OnTriggerEnter2D(Collider2D collision)
+        {
+            player = collision.gameObject.GetComponent<PlayerMovementController>();
+
+            if (player != null)
+            {
+                Explode();
+                return;
+            }
+
+            enemy = collision.gameObject.GetComponent<EnemyBehaviourController>();
+
+            if (enemy != null)
+            {
+                Explode();
+            }
+        }
+
+        public virtual void Explode()
+        {
+            explosionController = explosionsPool.GetObject();
+            explosionController.SetExplosionDamage(explosionDamage);
+            explosionController.SetExplosionRadius(explosionRadius);
+            explosionController.transform.position = transform.position;
+
+            gameObject.SetActive(false);
+        }
+    }
+}
