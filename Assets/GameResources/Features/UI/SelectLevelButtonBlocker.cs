88318ea9e41342655ec7c92cdb96afe9947f9e8d
MaminSiberian <120947225+msiberian42@users.noreply.github.com>
2024-02-12 12:39:09 +0300

    Сделал сохранение пройденных уровней

---
+++ Assets/GameResources/Features/UI/SelectLevelButtonBlocker.cs
@@ -0,0 +1,33 @@
+﻿namespace Tanks.Features.UI
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// Блокировщик кнопки загрузки уровня
+    /// </summary>
+    public class SelectLevelButtonBlocker : MonoBehaviour
+    {
+        [SerializeField]
+        protected int levelNumber = 1;
+
+        [SerializeField]
+        protected GameObject blocker = default;
+
+        protected int lastLevel = 0;
+
+        protected const string LAST_LEVEL = "Last passed level";
+
+        protected virtual void OnEnable()
+        {
+            if (!PlayerPrefs.HasKey(LAST_LEVEL))
+            {
+                PlayerPrefs.SetInt(LAST_LEVEL, 0);
+                PlayerPrefs.Save();
+            }
+
+            lastLevel = PlayerPrefs.GetInt(LAST_LEVEL);
+
+            blocker.SetActive(!(levelNumber <= lastLevel + 1));
+        }
+    }
+}
